e51d3a1ddabb27b5f66e9920751aff06e1fc9e52
Jake Moretz <jmoretz9090@gmail.com>
2023-10-30 16:39:44 -0400

    all functionality is complete, UI almost done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 
 const appSettings = {
@@ -12,12 +12,59 @@ const shoppingListInDb = ref(database, "shoppingList")
 
 const input = document.getElementById("input-field")
 const btn = document.getElementById("add-button")
+const shoppingList = document.getElementById("shopping-list")
 
+
+function clearInputValue() {
+    input.value = ""
+}
+
+function addItem(item) {
+    let currentId = item[0]
+    let currentValue = item[1]
+
+    let listElement = document.createElement("li")
+
+    listElement.textContent = currentValue
+
+    listElement.addEventListener("click", () => {
+        console.log(currentId)
+        let exactLocationOfItemInDb = ref(database, `shoppingList/${currentId}`)
+        remove(exactLocationOfItemInDb)
+    })
+    shoppingList.appendChild(listElement)
+}
+
+
+
+
+function clearShoppingList() {
+    shoppingList.innerHTML = ""
+}
 
 btn.addEventListener("click", () => {
     let inputValue = input.value
-    
     push(shoppingListInDb, inputValue)
-
-    console.log(inputValue)
+    clearInputValue()
 })
+
+onValue(shoppingListInDb, function(snapshot) {
+
+    if(snapshot.exists()){
+        let shoppingItemArray = Object.entries(snapshot.val())
+        clearShoppingList()
+
+            for (let i = 0; i < shoppingItemArray.length; i++) {
+            let currentItem = shoppingItemArray[i]
+            let currentItemId = currentItem[0]
+            let currentItemValue = currentItem[1]
+
+            addItem(currentItem)
+        }
+    } else {
+        shoppingList.innerHTML = "No items here yet..."
+    }
+ 
+
+}) 
+
